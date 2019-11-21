a1dc3ec1cff9020143f8feab1b45260231cc0cc8
rmtar <picpoint@yandex.ru>
2019-11-21 21:17:20 +0300

    work with gridfs

--- app.js
+++ app.js
@@ -1,15 +1,67 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const exphbs = require('express-handlebars');
+const bodyParser = require('body-parser');
+const crypto = require('crypto');
+const mongoose = require('mongoose');
+const multer = require('multer');
+const GridFsStorage = require('multer-gridfs-storage');
+const Grid = require('gridfs-stream');
+const methodOverride = require('method-override');
 const port = process.env.port || 4000;
-app.use(express.static(path.join('view')))
+const hbs = exphbs.create({
+  defaultLayout: 'main',
+  extname: 'hbs'
+});
+const urlMongoDB = 'mongodb+srv://rmtar:rmtar@cluster0-nw44p.mongodb.net/gridfsdb?retryWrites=true&w=majority';
 
-app.get('/about', (req, res) => {
-  res.sendFile(__dirname + '/view/about.html');
+app.use(bodyParser.json());
+app.use(methodOverride('_method'));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({extended: true}));
+app.engine('hbs', hbs.engine);
+app.set('view engine', 'hbs');
+app.set('views', 'views');
+
+const conn = mongoose.createConnection(urlMongoDB);
+
+let gfs;
+conn.once('open', () => {
+  gfs = Grid(conn.db, mongoose.mongo);
+  gfs.collection('gridfscollection');
+});
+
+const storage = new GridFsStorage({
+  url: urlMongoDB,
+  file: (req, file) => {
+    return new Promise((resolve, reject) => {
+      crypto.randomBytes(16, (err, buf) => {
+        if(err) {
+          reject(err);
+        } 
+        const filename = buf.toString('hex') + path.extname(file.originalname);
+        const fileInfo = {
+          filename: filename,
+          bucketName: 'gridfscollection'
+        };
+        resolve(fileInfo);
+      });
+    });
+  }
+});
+const uploads = multer({storage});
+
+app.post('/uploads', uploads.single('file'), (req, res) => {
+  res.json({file: req.file});
+});
+
+app.get('/uploads', (req, res) => {
+  res.render('uploads.hbs');
 });
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname, '/view/index.html');
+  res.render('index.hbs');
 });
 
 app.listen(port, (req, res) => {
